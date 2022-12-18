260219dfc89e349a50d82bd18786c50cb89f16f0
Kev <kkauf355@gmail.com>
2022-12-17 19:12:24 -0500

    roll new dice works. holdDie updates isHeld. Roll button needs to only re-roll dice that aren't held

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,61 @@
 import React from 'react'
 import './App.css'
 import Die from './components/Die'
+import { nanoid } from 'nanoid'
+
 
 function App() {
-  const [dice, setDice] = React.useState([1,1,1,1,1,1,1,1,1,1])
+  const [dice, setDice] = React.useState(allNewDice())
+
+  function allNewDice() {
+    const newDice = []
+    for (let i = 0; i < 10; i++) {
+      newDice.push({
+        id: nanoid(),
+        value: Math.ceil(Math.random() * 6), 
+        isHeld: false})
+    }
+    console.log(newDice) 
+    return newDice
+  }
+
+  function rollDice() {
+    console.log('rolling dice')
+    setDice(allNewDice())
+  }
+
+  function holdDie(id) {
+    setDice(
+      prevDice => prevDice.map(
+        die => die.id === id ? {...die, isHeld: !die.isHeld} : die
+      )
+    )
+
+    console.log(id)
+  }
+
+  const diceElements = dice.map((die, i)=> {
+    return (
+      <Die 
+        key={die.id}
+        value={die.value}
+        isHeld={die.isHeld}
+        holdDie={() => holdDie(die.id)}
+      />
+    )
+  })
 
   return (
-    <div className="App flex">
+    <main className="App flex">
       <div className="game-square flex">
-        <h1>Tenzies</h1>
-        <p>Roll until all dice are the same. Click each die to freez it at its current value between rolls</p>
-      
-          <Die />
-        
-      <button className='roll-button'>Roll</button>
+        <h1 className='title'>Tenzies</h1>
+        <p className='instructions flex'>Roll until all dice are the same. Click each die to freez it at its current value between rolls</p>
+        <div className='dice-area grid'>
+          {diceElements}
+        </div>
+        <button className='roll-button' onClick={rollDice}>Roll</button>
       </div>
-    </div>
+    </main>
   )
 }
 
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 
-export default function Die() {
-
-    function rollDie() {
-
+export default function Die(props) {
+    const styles = {
+            backgroundColor: props.isHeld ? "#59E391" : "white"
     }
 
     return (
-        <div className='Die'>
-            1
+        <div 
+            className="Die" 
+            style={styles}
+            onClick={props.holdDie}
+        >
+            {props.value}
         </div>
     )
 }
